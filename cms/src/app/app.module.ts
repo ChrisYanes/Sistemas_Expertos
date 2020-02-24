4f9088ce0718fc1b82fa10a9718ffba382ab83aa
ChrisYanes <batyaneschris@gmail.com>
2020-02-24 14:20:06 -0600

    Commit 2

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
