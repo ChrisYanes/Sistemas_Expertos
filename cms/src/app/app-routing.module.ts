f9a4491ab685f6bdc41a91b6d5bf685fce0c8297
ChrisYanes <batyaneschris@gmail.com>
2020-02-27 12:16:30 -0600

    Commit 3

--- cms/src/app/app-routing.module.ts
+++ cms/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { LoginComponent } from 'src/app/login/login.component';
+import { RegistroComponent } from 'src/app/registro/registro.component';
+//import {  } from 'src/app/';
+const routes: Routes = [
+  {
+    path:'registro',
+    component: RegistroComponent
+  },
+  
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  // {
+  //   path:'**',
+  //   component:NotFoundComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
