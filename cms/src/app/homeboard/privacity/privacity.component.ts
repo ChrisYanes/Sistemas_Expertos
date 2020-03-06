8010fe281d3157c3bb066ac0fb4b5746b50558b1
Christian Yanes <batyaneschris@gmail.com>
2020-03-05 23:48:59 -0600

    privacidad, seguridad terminados. iniciado el de compras

--- cms/src/app/homeboard/privacity/privacity.component.ts
+++ cms/src/app/homeboard/privacity/privacity.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any; 
+
 @Component({
   selector: 'app-privacity',
   templateUrl: './privacity.component.html',
@@ -10,6 +12,21 @@ export class PrivacityComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+    $( "input:checkbox" ).on("click",function(){
+      $("input[type=submit]").removeAttr("disabled"); 
+      if ($("input[type=submit]").removeAttr("disabled")==true){
+        $("input:checkbox").removeAttr("checked")
+      } else{
+        $("input:checkbox").removeAttr("unckecked");
+        $("input[type=submit]").Attr("disabled");
+      }
+      });
   }
 
+//   toggleDeshabilitar(){
+//     $( "input:checkbox" ).on("click",function(){
+//       $("input[type=submit]").removeAttr("disabled"); 
+//       });
+// }
 }
--- cms/src/app/homeboard/purchases/purchases.component.ts
+++ cms/src/app/homeboard/purchases/purchases.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-purchases',
   templateUrl: './purchases.component.html',
@@ -11,5 +11,48 @@ export class PurchasesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  showHideDiv(ele) {
+    var srcElement = document.getElementById(ele);
+    if (srcElement != null) {
+      if (srcElement.style.display == "block") {
+        srcElement.style.display = 'none';
+      }
+      else {
+        srcElement.style.display = 'block';
+        $('#det-planilla').hide();
+        $('#det-planilla2').hide();    
+        $('#divMsg2').hide();    
+      }
+      return false;
+    }
+  }
+
+  VolverMes(id){
+    $('#det-planilla').show();
+    $('#divMsg').hide();
+    $('#divMsg2').hide();
+  }
+
+  showHideDiv2(ele) {
+    var srcElement = document.getElementById(ele);
+    if (srcElement != null) {
+      if (srcElement.style.display == "block") {
+        srcElement.style.display = 'none';
+      }
+      else {
+        srcElement.style.display = 'block';
+        $('#det-planilla2').hide();
+        $('#det-planilla').hide();    
+      }
+      return false;
+    }
+  }
+
+  VolverMes2(id){
+    $('#det-planilla2').show();
+    $('#divMsg2').hide();
+  }
+
 
 }
